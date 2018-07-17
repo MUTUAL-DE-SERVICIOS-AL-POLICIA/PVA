ad38a462b387a53e8d41cc56d7de5bfc34188cb7
Daniel Jimenez Jerez <djimenezjerez@gmail.com>
2018-07-17 11:53:51 -0400

    Added database production seeder

---
+++ database/seeds/DatabaseProductionSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
+
+class DatabaseProductionSeeder extends Seeder
+{
+  /**
+   * Seed the application's database.
+   *
+   * @return void
+   */
+  public function run()
+  {
+    $this->command->info('Unguarding models');
+    Model::unguard();
+
+    $this->call(CitySeeder::class);
+    $this->call(InsuranceCompanySeeder::class);
+    $this->call(ManagementEntitySeeder::class);
+  }
+}
