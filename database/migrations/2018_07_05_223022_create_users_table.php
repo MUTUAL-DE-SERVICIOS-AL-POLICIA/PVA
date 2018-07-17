94e55c26deffb71f59b4db54621137a44b6e511f
Daniel Jimenez Jerez <djimenezjerez@gmail.com>
2018-07-17 08:55:04 -0400

    Use remeberToken function

--- database/migrations/2018_07_05_223022_create_users_table.php
+++ database/migrations/2018_07_05_223022_create_users_table.php
@@ -17,7 +17,7 @@ class CreateUsersTable extends Migration
       $table->increments('id');
       $table->string('username')->unique();
       $table->string('password');
-      $table->text('remember_token')->nullable();
+      $table->rememberToken();
       $table->timestamps();
     });
   }
