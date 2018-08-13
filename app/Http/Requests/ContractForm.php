8b8a7270e8281666554872854efeaa0563dcecdd
will <wian8887@gmail.com>
2018-08-13 10:39:58 -0400

    ADD contracts controller. close #21

---
+++ app/Http/Requests/ContractForm.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ContractForm extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'employee_id'      => 'required',
+            'position_id'      => 'required',
+            'contract_type_id' => 'required',
+            'contract_mode_id' => 'required',
+            'start_date'       => 'required',
+            'active'           => 'required',
+        ];
+    }
+    public function messages()
+    {
+        return [
+            'required' => 'No puede estar vacio',
+        ];
+    }
+}
