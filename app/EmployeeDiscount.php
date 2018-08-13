bf024c640aadf80c82e90b8d6b7d0b38591e8a91
Daniel Jimenez Jerez <djimenezjerez@gmail.com>
2018-08-13 18:17:42 -0400

    Added workdays to job_schedules

--- app/EmployeeDiscount.php
+++ app/EmployeeDiscount.php
@@ -7,7 +7,10 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 
 class EmployeeDiscount extends Model {
 	use SoftDeletes;
+	public $timestamps = true;
+	public $guarded = ['id'];
 	protected $dates = ['deleted_at'];
+	protected $fillable = ['elderly', 'common_risk', 'comission', 'solidary', 'national', 'rc_iva', 'faults', 'active'];
 
 	public function procedures() {
 		return $this->hasMany(Procedure::class);
--- app/EmployerContribution.php
+++ app/EmployerContribution.php
@@ -7,7 +7,10 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 
 class EmployerContribution extends Model {
 	use SoftDeletes;
+	public $timestamps = true;
+	public $guarded = ['id'];
 	protected $dates = ['deleted_at'];
+	protected $fillable = ['insurance_company', 'professional_risk', 'solidary', 'housing', 'active'];
 
 	public function procedures() {
 		return $this->hasMany(Procedure::class);
