77f41fd98cc1acb651f5e7622e210bc50aeb385d
Daniel Jimenez Jerez <djimenezjerez@gmail.com>
2018-08-14 10:34:55 -0400

    Resolved namespace, changed to Api/V1

--- app/Http/Controllers/Api/V1/ProcedureController.php
+++ app/Http/Controllers/Api/V1/ProcedureController.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Api\V1;
 
+use App\Http\Controllers\Controller;
 use App\Http\Requests\ProcedureForm;
 use App\Procedure;
 use Illuminate\Http\Request;
