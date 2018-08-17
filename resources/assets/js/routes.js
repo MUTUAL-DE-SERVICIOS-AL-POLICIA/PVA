cc348469f06ae3a7d2f616c0da2ef5a3618d01a2
will <wian8887@gmail.com>
2018-08-17 19:06:33 -0400

    ADD contracts

--- resources/assets/js/routes.js
+++ resources/assets/js/routes.js
@@ -2,35 +2,42 @@ import Home from './components/Home.vue'
 import Login from './components/auth/Login.vue'
 import Employee from './components/employee/EmployeeIndex.vue'
 import Company from './components/company/CompanyIndex.vue'
-
+import Contract from './components/contract/ContractIndex.vue'
 export const routes = [{
-  name: 'login',
-  path: '/login',
-  component: Login
+    name: 'login',
+    path: '/login',
+    component: Login
 }, {
-  name: 'home',
-  path: '/home',
-  component: Home,
-  meta: {
-    requiresAuth: true
-  }
+    name: 'home',
+    path: '/home',
+    component: Home,
+    meta: {
+        requiresAuth: true
+    }
 }, {
-  path: '*',
-  redirect: {
-    name: 'home'
-  }
+    path: '*',
+    redirect: {
+        name: 'home'
+    }
 }, {
-  path: '/employee',
-  component: Employee,
-  name: 'employeeIndex',
-  meta: {
-    requiresAuth: true
-  }
+    path: '/employee',
+    component: Employee,
+    name: 'employeeIndex',
+    meta: {
+        requiresAuth: true
+    }
 }, {
-  path: '/company',
-  component: Company,
-  name: 'companyIndex',
-  meta: {
-    requiresAuth: true
-  }
+    path: '/company',
+    component: Company,
+    name: 'companyIndex',
+    meta: {
+        requiresAuth: true
+    }
+}, {
+    path: '/contract',
+    component: Contract,
+    name: 'contractIndex',
+    meta: {
+        requiresAuth: true
+    }
 }]
