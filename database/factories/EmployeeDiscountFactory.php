879abce909baad1ea6e1bfcdc9992e9363e1b5e6
Daniel Jimenez Jerez <djimenezjerez@gmail.com>
2018-08-29 18:43:00 -0400

    Changed seeders for contributions and discounts

--- database/factories/EmployeeDiscountFactory.php
+++ database/factories/EmployeeDiscountFactory.php
@@ -4,12 +4,12 @@ use Faker\Generator as Faker;
 
 $factory->define(App\EmployeeDiscount::class, function (Faker $faker) {
 	return [
-		'elderly' => $faker->randomFloat($nbMaxDecimals = 2, $min = 0, $max = 99),
-		'common_risk' => $faker->randomFloat($nbMaxDecimals = 2, $min = 0, $max = 99),
-		'comission' => $faker->randomFloat($nbMaxDecimals = 2, $min = 0, $max = 99),
-		'solidary' => $faker->randomFloat($nbMaxDecimals = 2, $min = 0, $max = 99),
-		'national' => $faker->randomFloat($nbMaxDecimals = 2, $min = 0, $max = 99),
-		'rc_iva' => $faker->randomFloat($nbMaxDecimals = 2, $min = 0, $max = 99),
+		'elderly' => $faker->randomFloat($nbMaxDecimals = 3, $min = 0, $max = 0.1),
+		'common_risk' => $faker->randomFloat($nbMaxDecimals = 3, $min = 0, $max = 0.1),
+		'comission' => $faker->randomFloat($nbMaxDecimals = 3, $min = 0, $max = 0.1),
+		'solidary' => $faker->randomFloat($nbMaxDecimals = 3, $min = 0, $max = 0.1),
+		'national' => $faker->randomFloat($nbMaxDecimals = 3, $min = 0, $max = 0.1),
+		'rc_iva' => $faker->randomFloat($nbMaxDecimals = 3, $min = 0, $max = 0.1),
 		'active' => true,
 	];
 });
--- database/factories/EmployerContributionFactory.php
+++ database/factories/EmployerContributionFactory.php
@@ -4,10 +4,10 @@ use Faker\Generator as Faker;
 
 $factory->define(App\EmployerContribution::class, function (Faker $faker) {
 	return [
-		'insurance_company' => $faker->randomFloat($nbMaxDecimals = 2, $min = 0, $max = 99),
-		'professional_risk' => $faker->randomFloat($nbMaxDecimals = 2, $min = 0, $max = 99),
-		'solidary' => $faker->randomFloat($nbMaxDecimals = 2, $min = 0, $max = 99),
-		'housing' => $faker->randomFloat($nbMaxDecimals = 2, $min = 0, $max = 99),
+		'insurance_company' => $faker->randomFloat($nbMaxDecimals = 3, $min = 0, $max = 0.1),
+		'professional_risk' => $faker->randomFloat($nbMaxDecimals = 3, $min = 0, $max = 0.1),
+		'solidary' => $faker->randomFloat($nbMaxDecimals = 3, $min = 0, $max = 0.1),
+		'housing' => $faker->randomFloat($nbMaxDecimals = 3, $min = 0, $max = 0.1),
 		'active' => true,
 	];
 });
