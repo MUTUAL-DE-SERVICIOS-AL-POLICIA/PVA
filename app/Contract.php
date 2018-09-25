957a60bb9283496e3cfce81a933a20d04b07093f
Daniel Jimenez Jerez <djimenezjerez@gmail.com>
2018-09-25 08:45:27 -0400

    Fixed error on get valid contracts without retirement_date

--- app/Contract.php
+++ app/Contract.php
@@ -6,46 +6,56 @@ use Carbon;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class Contract extends Model {
+class Contract extends Model
+{
 	use SoftDeletes;
 	protected $dates = ['deleted_at'];
 	public $timestamps = true;
 	public $guarded = ['id'];
 	protected $fillable = ['insurance_company_id', 'employee_id', 'position_id', 'contract_type_id', 'contract_mode_id', 'start_date', 'end_date', 'retirement_date', 'retirement_reason_id', 'active', 'rrhh_cite', 'rrhh_cite_date', 'performance_cite', 'insurance_number', 'contract_number', 'hiring_reference_number', 'description'];
 
-	public function employee() {
+	public function employee()
+	{
 		return $this->belongsTo(Employee::class);
 	}
 
-	public function position() {
+	public function position()
+	{
 		return $this->belongsTo(Position::class);
 	}
 
-	public function contract_type() {
+	public function contract_type()
+	{
 		return $this->belongsTo(ContractType::class);
 	}
 
-	public function contract_mode() {
+	public function contract_mode()
+	{
 		return $this->belongsTo(ContractMode::class);
 	}
 
-	public function retirement_reason() {
+	public function retirement_reason()
+	{
 		return $this->belongsTo(RetirementReason::class);
 	}
 
-	public function job_schedules() {
+	public function job_schedules()
+	{
 		return $this->belongsToMany(JobSchedule::class);
 	}
 
-	public function payrolls() {
+	public function payrolls()
+	{
 		return $this->hasMany(Payroll::class);
 	}
 
-	public function insurance_company() {
+	public function insurance_company()
+	{
 		return $this->belongsTo(InsuranceCompany::class);
 	}
 
-	public function valid_date($year, $month) {
+	public function valid_date($year, $month)
+	{
 		$first_day_of_month = Carbon::create($year, $month, 1)->format('Y-m-d');
 		$last_day_of_month = Carbon::create($year, $month, 1)->endOfMonth()->format('Y-m-d');
 
@@ -63,15 +73,13 @@ class Contract extends Model {
 						->whereYear('retirement_date', $year)
 						->whereMonth('retirement_date', $month);
 				})
-				->orWhere(function ($q) use ($last_day_of_month, $first_day_of_month) {
+				->orWhere(function ($q) use ($year, $month) {
 					$q
-						->whereNull('retirement_date')
-						->whereDate('end_date', '>=', $first_day_of_month)
-						->whereDate('start_date', '<', $last_day_of_month);
+						->whereYear('end_date', $year)
+						->whereMonth('end_date', $month);
 				})
 				->orWhere(function ($q) use ($year, $month) {
 					$q
-						->whereNull('retirement_date')
 						->whereYear('start_date', $year)
 						->whereMonth('start_date', $month);
 				});
