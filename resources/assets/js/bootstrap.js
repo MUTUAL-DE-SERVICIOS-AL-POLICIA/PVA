2efec48027c3038a19c7722af12738914c6d32c1
Daniel Jimenez Jerez <djimenezjerez@gmail.com>
2018-11-14 10:21:00 -0400

    Disabled refresh page on F5 and ctrl+R

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -3,3 +3,19 @@ window.$ = window.jQuery = require('jquery')
 window.axios = require('axios').create({
   baseURL: process.env.MIX_APP_URL
 })
+
+let ctrlKeyDown = false
+$(document).ready(() => {
+  $(document).on("keydown", keydown)
+  $(document).on("keyup", keyup)
+})
+let keydown = e => {
+  if ((e.which || e.keyCode) == 116 || ((e.which || e.keyCode) == 82 && ctrlKeyDown)) {
+    e.preventDefault()
+  } else if ((e.which || e.keyCode) == 17) {
+    ctrlKeyDown = true
+  }
+}
+let keyup = e => {
+  if ((e.which || e.keyCode) == 17) ctrlKeyDown = false
+}
