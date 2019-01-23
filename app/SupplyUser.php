30d6623ee4b506e38bb706afc304f6ce64b527d1
Vrian7 <i-am-brian@hotmail.com>
2019-01-23 17:58:38 -0400

    delete trash

--- app/SupplyUser.php
+++ app/SupplyUser.php
@@ -8,4 +8,14 @@ class SupplyUser extends Model
 {
     protected $connection = 'nsiaf';
     protected $table = 'users';
+
+    public function requests()
+    {
+        return $this->hasMany(SupplyRequest::class, 'user_id');
+    }
+
+    public function department()
+    {
+        return $this->belongsTo(SupplyDepartment::class, 'department_id');
+    }
 }
