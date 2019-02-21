a85cd58c50d6af285de1f047690ec0261ece1c27
Daniel Jimenez Jerez <djimenezjerez@gmail.com>
2019-02-21 11:02:52 -0400

    Added barcode to departure printed form, closed #355

--- app/Http/Controllers/Api/V1/DepartureController.php
+++ app/Http/Controllers/Api/V1/DepartureController.php
@@ -19,217 +19,221 @@ use Illuminate\Http\Request;
  */
 class DepartureController extends Controller
 {
-    /**
+  /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
-    {
-        return Departure::with('contract', 'contract.employee', 'contract.employee.city_identity_card', 'contract.position', 'departure_reason.departure_type', 'departure_reason', 'certificate')
-            ->orderBy('created_at', 'DESC')
-            ->get();
-    }
-    /**
+  public function index()
+  {
+    return Departure::with('contract', 'contract.employee', 'contract.employee.city_identity_card', 'contract.position', 'departure_reason.departure_type', 'departure_reason', 'certificate')
+      ->orderBy('created_at', 'DESC')
+      ->get();
+  }
+  /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
-    {
-        if ($request->departure_time == null) {
-            $request->departure_time = '08:00';
-        }
-        if ($request->return_time == null) {
-            $request->return_time = '18:30';
-        }
-        $departure = Departure::create($request->all());
-        return $departure;
+  public function store(Request $request)
+  {
+    if ($request->departure_time == null) {
+      $request->departure_time = '08:00';
     }
+    if ($request->return_time == null) {
+      $request->return_time = '18:30';
+    }
+    $departure = Departure::create($request->all());
+    return $departure;
+  }
 
-    /**
+  /**
      * Display the specified resource.
      *
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
-    {
-        return Departure::findOrFail($id);
-    }
+  public function show($id)
+  {
+    return Departure::findOrFail($id);
+  }
 
-    /**
+  /**
      * Display the specified contract resource.
      *
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function get_departures($contract_id)
-    {
-        return Departure::with('contract', 'contract.employee', 'contract.employee.city_identity_card', 'contract.position', 'departure_reason.departure_type', 'departure_reason', 'certificate')
-            ->where('contract_id', $contract_id)
-            ->orderBy('created_at', 'DESC')
-            ->get();
-    }
+  public function get_departures($contract_id)
+  {
+    return Departure::with('contract', 'contract.employee', 'contract.employee.city_identity_card', 'contract.position', 'departure_reason.departure_type', 'departure_reason', 'certificate')
+      ->where('contract_id', $contract_id)
+      ->orderBy('created_at', 'DESC')
+      ->get();
+  }
 
-    /**
+  /**
      * Display the specified contract resource.
      *
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function get_departures_used($employee_id)
-    {
-        $month = Carbon::now()->month;
-        $year = Carbon::now()->year;
-        $hours = 0;
-        $days = 0;
-        $total_minutes_month = 0;
-        $total_minutes_year = 0;
-        $dep = [];
-        $contracts = Contract::where('employee_id', $employee_id)->get();
-        $departure_hour = DepartureReason::where('departure_type_id', 1)->where('name', 'Personal')->first();
-        $departure_day = DepartureReason::where('departure_type_id', 2)->where('name', 'Personal')->first();
-        foreach ($contracts as $contract) {
-            $departures = Departure::where('contract_id', $contract->id)->get();
-            foreach ($departures as $departure) {
-                $e = new EmployeeDeparture($departure);
-                $departure_days = Carbon::parse($departure->departure_date)->day;
-                $departure_year = Carbon::parse($departure->departure_date)->year;
-                if ($departure_days <= 19) {
-                    $departure_month1 = Carbon::parse($departure->departure_date)->subMonth()->month;
-                    $departure_month2 = Carbon::parse($departure->departure_date)->month;
-                } else {
-                    $departure_month1 = Carbon::parse($departure->departure_date)->month;
-                    $departure_month2 = Carbon::parse($departure->departure_date)->addMonth()->month;
-                }
-
-                $start_date = Carbon::parse($departure_year . '-0' . $departure_month1 . '-20')->format('Y-m-d');
-                $end_date = Carbon::parse($departure_year . '-' . $departure_month2 . '-19')->format('Y-m-d');
-                if ($departure->departure_date >= $start_date && $departure->departure_date <= $end_date) {
-                    if ($departure->departure_reason->departure_type_id == 1 && $departure->departure_reason->name == 'Personal' && $departure->approved == true) {
-                        $total_minutes_month = $total_minutes_month + $e->departure_minutes;
-                    }
-                }
-                if ($year == $departure_year) {
-                    if ($departure->departure_reason->departure_type_id == 2 && $departure->departure_reason->name == 'Personal' && $departure->approved == true) {
-                        // if ($departure->on_vacation != false) {
-                        $total_minutes_year = $total_minutes_year + $e->departure_minutes;
-                        // }
-                    }
-                }
-                $dep[] = $e;
-            }
+  public function get_departures_used($employee_id)
+  {
+    $month = Carbon::now()->month;
+    $year = Carbon::now()->year;
+    $hours = 0;
+    $days = 0;
+    $total_minutes_month = 0;
+    $total_minutes_year = 0;
+    $dep = [];
+    $contracts = Contract::where('employee_id', $employee_id)->get();
+    $departure_hour = DepartureReason::where('departure_type_id', 1)->where('name', 'Personal')->first();
+    $departure_day = DepartureReason::where('departure_type_id', 2)->where('name', 'Personal')->first();
+    foreach ($contracts as $contract) {
+      $departures = Departure::where('contract_id', $contract->id)->get();
+      foreach ($departures as $departure) {
+        $e = new EmployeeDeparture($departure);
+        $departure_days = Carbon::parse($departure->departure_date)->day;
+        $departure_year = Carbon::parse($departure->departure_date)->year;
+        if ($departure_days <= 19) {
+          $departure_month1 = Carbon::parse($departure->departure_date)->subMonth()->month;
+          $departure_month2 = Carbon::parse($departure->departure_date)->month;
+        } else {
+          $departure_month1 = Carbon::parse($departure->departure_date)->month;
+          $departure_month2 = Carbon::parse($departure->departure_date)->addMonth()->month;
         }
-        $total_departure['total_minutes_month'] = $total_minutes_month;
-        $total_departure['total_minutes_month_rest'] = $departure_hour->hour * 60 - $total_minutes_month;
-        $total_departure['total_minutes_year'] = $total_minutes_year;
-        $total_departure['total_minutes_year_rest'] = $departure_day->day * 8 * 60 - $total_minutes_year;
-        return $total_departure;
-    }
 
-    /**
+        $start_date = Carbon::parse($departure_year . '-0' . $departure_month1 . '-20')->format('Y-m-d');
+        $end_date = Carbon::parse($departure_year . '-' . $departure_month2 . '-19')->format('Y-m-d');
+        if ($departure->departure_date >= $start_date && $departure->departure_date <= $end_date) {
+          if ($departure->departure_reason->departure_type_id == 1 && $departure->departure_reason->name == 'Personal' && $departure->approved == true) {
+            $total_minutes_month = $total_minutes_month + $e->departure_minutes;
+          }
+        }
+        if ($year == $departure_year) {
+          if ($departure->departure_reason->departure_type_id == 2 && $departure->departure_reason->name == 'Personal' && $departure->approved == true) {
+            // if ($departure->on_vacation != false) {
+            $total_minutes_year = $total_minutes_year + $e->departure_minutes;
+            // }
+          }
+        }
+        $dep[] = $e;
+      }
+    }
+    $total_departure['total_minutes_month'] = $total_minutes_month;
+    $total_departure['total_minutes_month_rest'] = $departure_hour->hour * 60 - $total_minutes_month;
+    $total_departure['total_minutes_year'] = $total_minutes_year;
+    $total_departure['total_minutes_year_rest'] = $departure_day->day * 8 * 60 - $total_minutes_year;
+    return $total_departure;
+  }
+
+  /**
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
-    {
-        $departure = Departure::findOrFail($id);
-        $departure->fill($request->all());
-        $departure->save();
-        return $departure;
-    }
+  public function update(Request $request, $id)
+  {
+    $departure = Departure::findOrFail($id);
+    $departure->fill($request->all());
+    $departure->save();
+    return $departure;
+  }
 
-    /**
+  /**
      * Remove the specified resource from storage.
      *
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
-    {
-        $departure = Departure::findOrFail($id);
-        $departure->delete();
-        $certificate = Certificate::findOrFail($departure->certificate_id);
-        $certificate->delete();
-        return $departure;
-    }
+  public function destroy($id)
+  {
+    $departure = Departure::findOrFail($id);
+    $departure->delete();
+    $certificate = Certificate::findOrFail($departure->certificate_id);
+    $certificate->delete();
+    return $departure;
+  }
 
-    /**
+  /**
      * PDF the specified resource from storage.
      *
      * @param  int  $id
      * @return pdf
      */
-    function print($departure_id)
-    {
-        $pageWidth = '216';
-        $pageHeight = '279';
-        $pageMargins = [1, 2.5, 0, 2.5];
-        $pageName = 'Solicitud.pdf';
+  function print($departure_id)
+  {
+    $data = array('departure' => Departure::findOrFail($departure_id));
 
-        $data = [
-            'departure' => Departure::findOrFail($departure_id)
-        ];
-        return \PDF::loadView('departure.print', $data)
-            ->setOption('page-width', $pageWidth)
-            ->setOption('page-height', $pageHeight)
-            ->setOption('margin-top', $pageMargins[0])
-            ->setOption('margin-right', $pageMargins[1])
-            ->setOption('margin-bottom', $pageMargins[2])
-            ->setOption('margin-left', $pageMargins[3])
-            ->setOption('encoding', 'utf-8')
-            ->stream($pageName);
-    }
-    function print_report(Request $request)
-    {
-        $search['state'] = $request->state;
-        $search['type'] = $request->type;
-        $search['start_date'] = $request->start_date;
-        $search['end_date'] = $request->end_date;
+    $file_name = implode('_', ['solicitud', 'salida', $data['departure']->id]) . '.pdf';
 
-        $query = Departure::join('contracts', 'contracts.id', '=', 'departures.contract_id')
-            ->join('positions', 'positions.id', '=', 'contracts.position_id')
-            ->join('departure_reasons', 'departure_reasons.id', '=', 'departures.departure_reason_id')
-            ->whereBetween('departures.departure_date', [$request->start_date, $request->end_date]);
-        if ($request->position_group_id != null) {
-            $query->where('positions.position_group_id', $request->position_group_id);
-            $search['position_group'] = PositionGroup::findOrFail($request->position_group_id);
-        }
-        if ($request->employee_id != null) {
-            $query->where('contracts.employee_id', $request->employee_id);
-            $search['employee'] = Employee::findOrFail($request->employee_id);
-        }
-        if ($request->state != null) {
-            $query->where('departures.approved', $request->state);
-        }
-        if ($request->type != null) {
-            $query->where('departure_reasons.departure_type_id', $request->type);
-        }
-        $res = $query->get();
-        $pageWidth = '216';
-        $pageHeight = '279';
-        $pageMargins = [10, 10, 10, 15];
-        $pageName = 'Reporte de salidas/licencias.pdf';
-        $data = [
-            'departures' => $res,
-            'search' => (object)$search
-        ];
-        return \PDF::loadView('departure.report', $data)
-            ->setOption('page-width', $pageWidth)
-            ->setOption('page-height', $pageHeight)
-            ->setOption('margin-top', $pageMargins[0])
-            ->setOption('margin-right', $pageMargins[1])
-            ->setOption('margin-bottom', $pageMargins[2])
-            ->setOption('margin-left', $pageMargins[3])
-            ->setOption('footer-font-size', 5)
-            ->setOption('footer-center', '[page] de [topage] - Impreso el ' . date('m/d/Y H:i'))
-            ->setOption('encoding', 'utf-8')
-            ->setOption('user-style-sheet', public_path('css/report-print.min.css'))
-            ->stream($pageName);
+    $options = [
+      'orientation' => 'portrait',
+      'page-width' => '216',
+      'page-height' => '356',
+      'margin-top' => '1',
+      'margin-bottom' => '0',
+      'margin-left' => '2.5',
+      'margin-right' => '2.5',
+      'encoding' => 'UTF-8',
+      'user-style-sheet' => public_path('css/report-print.min.css')
+    ];
+
+    $pdf = \PDF::loadView('departure.print', $data);
+    $pdf->setOptions($options);
+
+    return $pdf->stream($file_name);
+  }
+
+  function print_report(Request $request)
+  {
+    $search['state'] = $request->state;
+    $search['type'] = $request->type;
+    $search['start_date'] = $request->start_date;
+    $search['end_date'] = $request->end_date;
+
+    $query = Departure::join('contracts', 'contracts.id', '=', 'departures.contract_id')
+      ->join('positions', 'positions.id', '=', 'contracts.position_id')
+      ->join('departure_reasons', 'departure_reasons.id', '=', 'departures.departure_reason_id')
+      ->whereBetween('departures.departure_date', [$request->start_date, $request->end_date]);
+    if ($request->position_group_id != null) {
+      $query->where('positions.position_group_id', $request->position_group_id);
+      $search['position_group'] = PositionGroup::findOrFail($request->position_group_id);
     }
+    if ($request->employee_id != null) {
+      $query->where('contracts.employee_id', $request->employee_id);
+      $search['employee'] = Employee::findOrFail($request->employee_id);
+    }
+    if ($request->state != null) {
+      $query->where('departures.approved', $request->state);
+    }
+    if ($request->type != null) {
+      $query->where('departure_reasons.departure_type_id', $request->type);
+    }
+    $res = $query->get();
+    $pageWidth = '216';
+    $pageHeight = '279';
+    $pageMargins = [10, 10, 10, 15];
+    $pageName = 'Reporte de salidas/licencias.pdf';
+    $data = [
+      'departures' => $res,
+      'search' => (object)$search
+    ];
+    return \PDF::loadView('departure.report', $data)
+      ->setOption('page-width', $pageWidth)
+      ->setOption('page-height', $pageHeight)
+      ->setOption('margin-top', $pageMargins[0])
+      ->setOption('margin-right', $pageMargins[1])
+      ->setOption('margin-bottom', $pageMargins[2])
+      ->setOption('margin-left', $pageMargins[3])
+      ->setOption('footer-font-size', 5)
+      ->setOption('footer-center', '[page] de [topage] - Impreso el ' . date('m/d/Y H:i'))
+      ->setOption('encoding', 'utf-8')
+      ->setOption('user-style-sheet', public_path('css/report-print.min.css'))
+      ->stream($pageName);
+  }
 }
--- resources/views/departure/print.blade.php
+++ resources/views/departure/print.blade.php
@@ -1,51 +1,59 @@
+<?php
+use \Carbon\Carbon;
+use \Milon\Barcode\DNS2D;
+?>
+
 <!DOCTYPE html>
 <html lang="es">
-<head>
-  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-  <title>PLATAFORMA VIRTUAL - MUSERPOL </title>
-  <link rel="stylesheet" href="{{ public_path("/css/report-print.min.css") }}" media="all" />
-  <style>
-    .data-row {
-      height: 17px;
-      min-height: 17px;
-    }
-    .description-row {
-      height: 138px;
-      min-height: 138px;
-    }
-    .scissors-rule {
-      display: block;
-      text-align: center;
-      overflow: hidden;
-      white-space: nowrap;
-    }
-    .scissors-rule > span {
-      position: relative;
-      display: inline-block;
-    }
-    .scissors-rule > span:before,
-    .scissors-rule > span:after {
-      content: "";
-      position: absolute;
-      top: 50%;
-      width: 9999px;
-      height: 1px;
-      background: white;
-      border-top: 1px dashed black;
-    }
-    .scissors-rule > span:before {
-      right: 100%;
-      margin-right: 5px;
-    }
-    .scissors-rule > span:after {
-      left: 100%;
-      margin-left: 5px;
-    }
-  </style>
-</head>
-<body style="border: 0; border-radius: 0;">
-  @for($i = 0; $i < 3; $i++)
-  <div class="page-break px-4" style="border-radius: 0.75em; border: 1px solid #22292f; padding-top: 3px;">
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <title>PLATAFORMA VIRTUAL - MUSERPOL</title>
+    <link rel="stylesheet" href="{{ public_path("/css/report-print.min.css") }}" media="all" />
+    <style>
+      .data-row {
+        height: 17px;
+        min-height: 17px;
+      }
+      .description-row {
+        height: 138px;
+        min-height: 138px;
+      }
+      .scissors-rule {
+        display: block;
+        text-align: center;
+        overflow: hidden;
+        white-space: nowrap;
+        margin-top: 0px;
+        margin-bottom: 3px;
+      }
+      .scissors-rule > span {
+        position: relative;
+        display: inline-block;
+      }
+      .scissors-rule > span:before,
+      .scissors-rule > span:after {
+        content: "";
+        position: absolute;
+        top: 50%;
+        width: 9999px;
+        height: 1px;
+        background: white;
+        border-top: 1px dashed black;
+      }
+      .scissors-rule > span:before {
+        right: 100%;
+        margin-right: 5px;
+      }
+      .scissors-rule > span:after {
+        left: 100%;
+        margin-left: 5px;
+      }
+    </style>
+  </head>
+
+  <body style="border: 0; border-radius: 0;">
+    @for($i = 0; $i < 3; $i++)
+    <div class="page-break px-4" style="border-radius: 0.75em; border: 1px solid #22292f; padding-top: 3px;">
     <table class="w-100">
       <tr>
         <th class="w-15 text-left no-padding no-margins align-middle">
@@ -54,7 +62,7 @@
           </div>
         </th>
         <th class="w-50 align-top">
-          <div class="font-thin uppercase leading-tight text-xs" >
+          <div class="font-thin uppercase leading-tight text-xs">
             <div>MUTUAL DE SERVICIOS AL POLICÍA "MUSERPOL"</div>
             <div>DIRECCIÓN DE ASUNTOS ADMINISTRATIVOS</div>
             <div>UNIDAD DE RECURSOS HUMANOS</div>
@@ -73,13 +81,16 @@
               </tr>
               <tr>
                 <td class="text-center bg-grey-darker text-white">Fecha</td>
-                <td class="uppercase">{{ Carbon::parse($departure->created_at)->format('d/m/Y') }}</td>
+                @php ($date = Carbon::parse($departure->created_at)->format('d/m/Y'))
+                <td class="uppercase">{{ $date }}</td>
               </tr>
             </tbody>
           </table>
         </th>
       </tr>
-      <tr><td colspan="3" style="border-top: 1px solid #22292f;"></td></tr>
+      <tr>
+        <td colspan="3" style="border-top: 1px solid #22292f;"></td>
+      </tr>
     </table>
     <div class="block">
       <div class="font-semibold leading-tight text-xs text-center m-b-5">SOLICITUD DE PERMISO</div>
@@ -89,19 +100,19 @@
         </tr>
         <tr>
           @php ($name = Util::fullName($departure->contract->employee, 'uppercase'))
-          <td class="{{ Util::departure_string_length($name) }} data-row">{{ $name }}</td>
+          <td class="{{ Util::departure_string_length($name) }} data-row py-5">{{ $name }}</td>
         </tr>
         <tr class="bg-grey-darker text-xxs text-white">
           <td>CARGO</td>
         </tr>
         <tr>
-          <td class="{{ Util::departure_string_length($departure->contract->position->name) }} data-row">{{ $departure->contract->position->name }}</td>
+          <td class="{{ Util::departure_string_length($departure->contract->position->name) }} data-row py-5">{{ $departure->contract->position->name }}</td>
         </tr>
         <tr class="bg-grey-darker text-xxs text-white">
           <td>ÁREA</td>
         </tr>
         <tr>
-          <td class="{{ Util::departure_string_length($departure->contract->position->position_group->name) }} data-row">{{ $departure->contract->position->position_group->name }}</td>
+          <td class="{{ Util::departure_string_length($departure->contract->position->position_group->name) }} data-row py-5">{{ $departure->contract->position->position_group->name }}</td>
         </tr>
       </table>
       <table class="table-info w-49 m-b-5 text-center uppercase" style="float: right; margin-left: 1px;">
@@ -109,25 +120,25 @@
           <td class="w-50" colspan='2'>MOTIVO</td>
         </tr>
         <tr>
-          <td class="w-50 text-xs " colspan='2'>{{ $departure->departure_reason->name }}</td>
+          <td class="w-50 text-xs data-row py-5" colspan='2'>{{ $departure->departure_reason->name }}</td>
         </tr>
         <tr class="bg-grey-darker text-xxs text-white">
           <td colspan='2'>DESTINO</td>
         </tr>
         <tr>
-          <td class="{{ Util::departure_string_length($departure->destiny) }} data-row" colspan='2'>{{ $departure->destiny }}</td>
+          <td class="{{ Util::departure_string_length($departure->destiny) }} data-row py-5" colspan='2'>{{ $departure->destiny }}</td>
         </tr>
         <tr class="bg-grey-darker text-xxs text-white">
           <td class="w-50">DESDE</td>
           <td class="w-50">HASTA</td>
         </tr>
-        <tr>
-          <td class="w-50 text-xs">
+        <tr class="text-xs">
+          <td class="w-50 py-5">
             <span>{{ Carbon::parse($departure->departure_date)->format('d/m/Y') }}</span>
             <span>&nbsp;</span>
             <span>{{ Carbon::parse($departure->departure_time)->format('H:i') }}</span>
           </td>
-          <td class="w-50 text-xs">
+          <td class="w-50 py-5">
             <span>{{ Carbon::parse($departure->return_date)->format('d/m/Y') }}</span>
             <span>&nbsp;</span>
             <span>{{ Carbon::parse($departure->return_time)->format('H:i') }}</span>
@@ -148,25 +159,35 @@
       </table>
       <table class="w-100 description-row" border="1" frame="void" rules="all">
         <tbody>
-          <tr style="height: 97px; vertical-align: bottom;">
-            <td class="text-center w-33 font-bold text-xxxs">Solicitante</td>
-            <td class="text-center w-33 font-bold text-xxxs">Autorizado</td>
-            <td class="text-center w-33 font-bold text-xxxs">RRHH</td>
+          <tr style="height: 176px; vertical-align: bottom;">
+            <td class="text-center w-25 font-bold text-xxxs">Solicitante</td>
+            <td class="text-center w-25 font-bold text-xxxs">Autorizado</td>
+            <td class="text-center w-25 font-bold text-xxxs">RRHH</td>
+            <td class="text-center w-25 font-bold text-xxxs">Destino</td>
           </tr>
         </tbody>
       </table>
     </div>
-  </div>
-  <table class="w-100 text-xxxxs">
-    <tr>
-      <td align="left">PLATAFORMA VIRTUAL ADMINISTRATIVA - MUSERPOL</td>
-    </tr>
-  </table>
-  @if($i != 2)
-  <div class="scissors-rule">
-    <span>&#9986;</span>
-  </div>
-  @endif
-  @endfor
-</body>
+    </div>
+    <table>
+      <tr>
+        <td class="text-xxxs" align="left">
+          @if (env("APP_ENV") == "production")
+            VERSIÓN DE PRUEBAS
+          @else
+            Este documento oficial queda inválido al contener rayaduras, correcciones o raspones
+          @endif
+        </td>
+        <td class="child" align="right">
+          <img src="data:image/png;base64, {{ DNS2D::getBarcodePNG(bcrypt($date . ' ' . gethostname() . ' ' . env('APP_URL')), 'PDF417') }}" alt="BARCODE!!!" style="height: 20px; width: 125px;" />
+        </td>
+      </tr>
+    </table>
+    @if($i != 2)
+    <div class="scissors-rule">
+      <span>&#9986;</span>
+    </div>
+    @endif
+    @endfor
+  </body>
 </html>
