5b5fd6277571728eafd160ddbdddc56c949721c9
Ale Guis F <aleguisf@gmail.com>
2023-04-11 12:08:02 -0400

    update MAE

--- resources/views/departure/note.blade.php
+++ resources/views/departure/note.blade.php
@@ -52,7 +52,7 @@ $consultant = $departure->employee->consultant();
         Señor:
       </div>
       <div>
-        Cnl. DESP. EDGAR JOSE CORTEZ ALBORNOZ
+        CNL. MSc. CAD. LUCIO ENRIQUE RENÉ JIMÉNEZ VARGAS
         <!--Cnl. Desp. {{ App\Position::with(['contracts' => function ($query) { $query->orderBy('created_at', 'ASC')->with('employee')->first(); }])->orderBy('created_at', 'ASC')->first()->contracts[0]->employee->fullName() }}-->
       </div>
       <div class="font-bold">
