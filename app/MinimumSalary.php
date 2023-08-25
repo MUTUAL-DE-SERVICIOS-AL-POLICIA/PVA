375bf4a3ca351aee4d7d9a90c26c091a906a54f8
gladys850 <gpq850@gmail.com>
2023-08-25 14:31:46 +0000

    bug salario minimo

--- app/MinimumSalary.php
+++ app/MinimumSalary.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class MinimumSalary extends Model
 {
-  public $timestamps = true;
+  public $timestamps = false;
   public $guarded = ['id'];
   protected $fillable = ['value', 'year', 'active'];
 }
