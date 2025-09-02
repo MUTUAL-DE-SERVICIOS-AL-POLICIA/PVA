36cf9258d0d1c9ad418d6787a8e1708fe6c16e25
Richard Velasquez <revelasquez666@gmail.com>
2025-09-02 12:52:05 -0400

    procedimientos faltantes en AssistantContractController

--- app/Http/Controllers/Api/V1/AssistantContractController.php
+++ app/Http/Controllers/Api/V1/AssistantContractController.php
@@ -18,29 +18,10 @@ class AssistantContractController extends Controller
     }
 
     public function store(Request $request)
-{
-    try {
-        DB::beginTransaction();
-        $validated = $request->validate([
-            'schedules.id' => 'nullable|integer|min:0',
-        ]);
-
-        $scheduleId = (int) data_get($validated, 'schedules.id', 0);
-        $data = $request->except('schedules', 'id');
-        $contract = AssistantContract::create($data);
-        $toAttach = ($scheduleId === 0) ? [1, 2] : [$scheduleId];
-        if (!empty($toAttach)) {
-            $contract->job_schedules()->syncWithoutDetaching(array_unique($toAttach));
-        }
-        DB::commit();
-        return response()->json($contract->load('job_schedules'), 201);
-    } catch (\Throwable $e) {
-        DB::rollBack();
-        return response()->json([
-            'error' => 'Error al crear el contrato: '.$e->getMessage(),
-        ], 500);
+    {
+        $contract = AssistantContract::create($request->all());
+        return $contract;
     }
-}
 
 
     public function show($id)
@@ -54,12 +35,13 @@ class AssistantContractController extends Controller
         $contract = AssistantContract::findOrFail($id);
         $contract->fill($request->all());
         $contract->save();
-        if ($request->schedules) {
-            $contract->job_schedules()->detach();
-            foreach ($request->schedules as $schedule) {
-                $contract->job_schedules()->attach($schedule);
-            }return "lasjkdh";
-        }
+        return $contract;
+    }
+
+    public function delete($id)
+    {
+        $contract = AssistantContract::findOrFail($id);
+        $contract->delete();
         return $contract;
     }
 }
